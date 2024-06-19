8a6fe06ff397b7ec3428ddb48c53712451927e52
AnantNathMishra <mishraanantnath3@gmail.com>
2024-06-19 07:07:50 +0530

    music web page

---
+++ script.js
@@ -0,0 +1,263 @@
+var songs = [
+    {songName : "Arjan Valley", artistName:"Bhupinder Babbal",poster:"./poster/animal.jpg",url:"./Songs/Arjan Vailly Ne.mp3"},
+    {songName : "Chann Sitare", artistName:"Ammy Virk",poster:"./poster/chann-sitare.jpg",url:"./Songs/Chann Sitare_320(PagalWorld.com.se).mp3"},
+    {songName : "Daku", artistName:"Inderpal Moga",poster:"./poster/daku.jpg",url:"./Songs/Daku_320(PagalWorld.com.se).mp3"},
+    {songName : "Excuses", artistName:"A P Dhillon",poster:"./poster/excuses.jpg",url:"./Songs/excuses.mp3"},
+    {songName : "Gatividhi", artistName:"Honey Singh",poster:"./poster/gatividhi.jpg",url:"./Songs/Gatividhi_320(PagalWorld.com.se).mp3"},
+    {songName : "Jaadugar", artistName:"Paradox",poster:"./poster/jadoogar.jpg",url:"./Songs/Jaadugar_320(PagalWorld.com.se).mp3"},
+    {songName : "Jale 2", artistName:"Sapna Chaudhary",poster:"./poster/jale.jpg",url:"./Songs/Jale 2.mp3"},
+    {songName : "Let Me Down Slowly x Main Dhoondne Ko Zamaane Mein", artistName:"D J Vikas",poster:"./poster/let-me-down-slowly.jpg",url:"./Songs/Let Me Down Slowly x Main Dhoondne Ko Zamaane Mein (Gravero Mashup)_320(PagalWorld.com.se).mp3"},
+    {songName : "Maan Meri Jaan", artistName:"D J Mix",poster:"./poster/man-meri-jaan.jpg",url:"./Songs/Maan Meri Jaan_320(PagalWorld.com.se).mp3"},
+    {songName : "Main Teri Mehbooba", artistName:"Yash Ft. Annanya Bhatt",poster:"./poster/mehbooba.jpg",url:"./Songs/Mehbooba Main Teri Mehbooba (KGF Chapter 2)_320(PagalWorld.com.se).mp3"},
+    {songName : "Pehle Bhi Main", artistName:"Vishal Mishra",poster:"./poster/animal.jpg",url:"./Songs/Pehle Bhi Main.mp3"},
+    {songName : "Ram Ram", artistName:"MC Square",poster:"./poster/ram-ram.jpg",url:"./Songs/Ram Ram_320(PagalWorld.com.se).mp3"},
+    {songName : "Ram Siya Ram", artistName:"Sachet-parampra",poster:"./poster/ram.jpg",url:"./Songs/Ram Siya Ram.mp3"},
+    {songName : "Blue Eyes", artistName:"Honey Singh",poster:"./poster/Blue-Eyes.jpg",url:"./Songs/Blue-Eyes.mp3"},
+]
+
+var popularArtist = [
+    {artistName:"Honey Singh", img:"./honey-singh.jpg" , song:[
+        {songName:"Love Dose",artistName:"Honey Singh",poster:"./Honey-Singh/Posters/Love-Dose.jpg",url:"./Honey-Singh/Songs/Love-Dose.mp3"},
+        {songName:"Daftar ki Girl",artistName:"Honey Singh",poster:"./Honey-Singh/Posters/daftar-ki-girl.jpg",url:"./Honey-Singh/Songs/Daftar-Ki-girl.mp3"},
+        {songName:"Ankhon Ankhon",artistName:"Honey Singh",poster:"./Honey-Singh/Posters/Bhaag-Johnny.jpg",url:"./Honey-Singh/Songs/Aankhon-Aankhon.mp3"},
+        {songName:"Angreji Beat",artistName:"Honey Singh",poster:"./Honey-Singh/Posters/Angreji-Beat.jpg",url:"./Honey-Singh/Songs/Angreji-Beat.mp3"},
+        {songName:"Birthday bash",artistName:"Honey Singh",poster:"./Honey-Singh/Posters/Birthday-Bash.jpg",url:"./Honey-Singh/Songs/Birthday-Bash.mp3"},
+        {songName:"Blue Eyes",artistName:"Honey Singh",poster:"./Honey-Singh/Posters/Blue-Eyes.jpg",url:"./Honey-Singh/Songs/Blue-Eyes.mp3"},
+        {songName:"Choot Vol1",artistName:"Honey Singh",poster:"./Honey-Singh/Posters/Choot-Vol-1.jpg",url:"./Honey-Singh/Songs/Choot-Vol-1.mp3"},
+        {songName:"Dheere Dheere",artistName:"Honey Singh",poster:"./Honey-Singh/Posters/Dheere-Dhere.jpg",url:"./Honey-Singh/Songs/Dheere-Dheere.mp3"},
+        {songName:"Glassi",artistName:"Honey Singh",poster:"./Honey-Singh/Posters/Glassy.jpg",url:"./Honey-Singh/Songs/Glassi.mp3"},
+        {songName:"High Heels",artistName:"Honey Singh",poster:"./Honey-Singh/Posters/High-Heels.jpg",url:"./Honey-Singh/Songs/High-Heels.mp3"},
+        {songName:"Issey Khetey Hai Hip Hop",artistName:"Honey Singh",poster:"./Honey-Singh/Posters/hip-hop.jpg",url:"./Honey-Singh/Songs/Issey-Kehte-Hai-Hip-Hop.mp3"},
+        {songName:"Lak 28 Kudi Da",artistName:"Honey Singh",poster:"./Honey-Singh/Posters/Lak-28-Kudi-Da.jpg",url:"./Honey-Singh/Songs/Lak-28-Kudi-Da-47.mp3"},
+        {songName:"One Bottle Down",artistName:"Honey Singh",poster:"./Honey-Singh/Posters/One-Bottle-Down.jpg",url:"./Honey-Singh/Songs/One-Bottle-down.mp3"},
+        {songName:"Party With BoothNath",artistName:"Honey Singh",poster:"./Honey-Singh/Posters/Party-With-Boothnath.jpg",url:"./Honey-Singh/Songs/Party-With-the-Bhoothnath.mp3"},
+        {songName:"Raat Jashan Di",artistName:"Honey Singh",poster:"./Honey-Singh/Posters/Raat-Jashan-Di.jpg",url:"./Honey-Singh/Songs/Raat-Jashan-Di.mp3"},
+        {songName:"Yaar Bathere",artistName:"Honey Singh",poster:"./Honey-Singh/Posters/yaar-bathere.jpg",url:"./Honey-Singh/Songs/Yaar-Bathere.mp3"},
+        {songName:"Yaar Na Miley",artistName:"Honey Singh",poster:"./Honey-Singh/Posters/yaar-na-miley.jpg",url:"./Honey-Singh/Songs/Yaar-Naa-miley.mp3"},
+    ]},
+    {artistName:"Guru Randhawa", img:"./guru-randhawa.jpg",song:[
+        {songName:"High Rated Gabru",artistName:"Guru Randhawa",poster:"./Guru-Randhawa/Posters/high-rated.jpg",url:"./Guru-Randhawa/Songs/High-Rated-Gabru.mp3"},
+        {songName:"I Like You",artistName:"Guru Randhawa",poster:"./Guru-Randhawa/Posters/I-Like-You.jpg",url:"./Guru-Randhawa/Songs/I-Like-You.mp3"},
+        {songName:"Khat",artistName:"Guru Randhawa",poster:"./Guru-Randhawa/Posters/Khat.jpg",url:"./Guru-Randhawa/Songs/Khat.mp3"},
+        {songName:"Lagdi hai Thai",artistName:"Guru Randhawa",poster:"./Guru-Randhawa/Posters/lgti-hai-thai.jpg",url:"./Guru-Randhawa/Songs/Lagdi-Hai-Thaai.mp3"},
+        {songName:"Lahore",artistName:"Guru Randhawa",poster:"./Guru-Randhawa/Posters/lahore.jpg",url:"./Guru-Randhawa/Songs/Lahore.mp3"},
+        {songName:"Made In India",artistName:"Guru Randhawa",poster:"./Guru-Randhawa/Posters/made-in-india.jpg",url:"./Guru-Randhawa/Songs/Made-In_india.mp3"},
+        {songName:"Patola",artistName:"Guru Randhawa",poster:"./Guru-Randhawa/Posters/patola.jpg",url:"./Guru-Randhawa/Songs/Patola.mp3"},
+        {songName:"Raat Kamaal hi",artistName:"Guru Randhawa",poster:"./Guru-Randhawa/Posters/Raat-Kamaal-Hai.jpg",url:"./Guru-Randhawa/Songs/Raat-Kamaal-Hai.mp3"},
+        {songName:"Suit",artistName:"Guru Randhawa",poster:"./Guru-Randhawa/Posters/Suit.jpg",url:"./Guru-Randhawa/Songs/Suit.mp3"},
+        {songName:"Tu Meri Rani",artistName:"Guru Randhawa",poster:"./Guru-Randhawa/Posters/Tu-Meri-Rani.jpg",url:"./Guru-Randhawa/Songs/Tu-Meri-Rani.mp3"},
+        {songName:"Yaar Mod Do",artistName:"Guru Randhawa",poster:"./Guru-Randhawa/Posters/yaar-mod-do.jpg",url:"./Guru-Randhawa/Songs/Yaar-Mod-Do.mp3"},
+    ]},
+    {artistName:"M C Square", img:"./MC-Square.jpg",song:[
+        {songName:"2 Woofer",artistName:"M C Square",poster:"./M-C-Square/Posters/2-woofer.jpg",url:"./M-C-Square/Songs/2-Woofer.mp3"},
+        {songName:"4 Din",artistName:"M C Square",poster:"./M-C-Square/Posters/4-din.jpg",url:"./M-C-Square/Songs/4-Din.mp3"},
+        {songName:"101",artistName:"M C Square",poster:"./M-C-Square/Posters/101.jpg",url:"./M-C-Square/Songs/101.mp3"},
+        {songName:"Aadha Gyaan",artistName:"M C Square",poster:"./M-C-Square/Posters/adha-gyan.jpg",url:"./M-C-Square/Songs/Aadha-Gyaan.mp3"},
+        {songName:"Badmos Chora",artistName:"M C Square",poster:"./M-C-Square/Posters/badmash-chora.jpg",url:"./M-C-Square/Songs/Badmos-Chora.mp3"},
+        {songName:"Bhoj",artistName:"M C Square",poster:"./M-C-Square/Posters/Bhoj.jpg",url:"./M-C-Square/Songs/Bhoj.mp3"},
+        {songName:"Chehre",artistName:"M C Square",poster:"./M-C-Square/Posters/Chehre.jpg",url:"./M-C-Square/Songs/Chehre.mp3"},
+        {songName:"Chhore NCR Aale",artistName:"M C Square",poster:"./M-C-Square/Posters/Chhore-NCR.jpg",url:"./M-C-Square/Songs/Chhore-NCR-Aale.mp3"},
+        {songName:"Naina Ki Talwar",artistName:"M C Square",poster:"./M-C-Square/Posters/naina-ki-talwar.jpg",url:"./M-C-Square/Songs/Naina-Ki-Talwar.mp3"},
+        {songName:"Rom Rom",artistName:"M C Square",poster:"./M-C-Square/Posters/rom-rom.jpg",url:"./M-C-Square/Songs/Ram-Ram.mp3"},
+    ]},
+    {artistName:"Paradox", img:"./Paradox.jpeg",song:[
+        {songName:"Babam Bam",artistName:"Paradox",poster:"./ParaDox/Posters/Babam-Bam.jpg",url:"./ParaDox/Songs/Babam-Bam.mp3"},
+        {songName:"Bt Ho Gayi",artistName:"Paradox",poster:"./ParaDox/Posters/bt-ho-gyi.jpg",url:"./ParaDox/Songs/Bt-Ho_Gayi.mp3"},
+        {songName:"Chhore NCR Aale",artistName:"Paradox",poster:"./ParaDox/Posters/chore-ncr-aale.jpg",url:"./ParaDox/Songs/Chhore-NCR-Aale.mp3"},
+        {songName:"Cowboy",artistName:"Paradox",poster:"./ParaDox/Posters/Cowboy.jpg",url:"./ParaDox/Songs/Cowboy.mp3"},
+        {songName:"Gang Wale Munde",artistName:"Paradox",poster:"./ParaDox/Posters/gang-wale-munde.jpg",url:"./ParaDox/Songs/Gang-Wale-Munde.mp3"},
+        {songName:"Glitch",artistName:"Paradox",poster:"./ParaDox/Posters/glitch.jpg",url:"./ParaDox/Songs/Glitch.mp3"},
+        {songName:"Itni Shakti",artistName:"Paradox",poster:"./ParaDox/Posters/itni-shakti.jpg",url:"./ParaDox/Songs/Itni-Shakti.mp3"},
+        {songName:"Jadugar",artistName:"Paradox",poster:"./ParaDox/Posters/jadugar.jpg",url:"./ParaDox/Songs/Jaadugar.mp3"},
+        {songName:"Mileya ni main",artistName:"Paradox",poster:"./ParaDox/Posters/Mileya-ni.jpg",url:"./ParaDox/Songs/MIleya-ni.mp3"},
+        {songName:"Rihaayi",artistName:"Paradox",poster:"./ParaDox/Posters/Rihaayi.jpg",url:"./ParaDox/Songs/Rihaayi.mp3"},
+    ]},
+    {artistName:"Badshah", img:"./Badshah.jpg",song:[
+        {songName:"D J Wale Babu",artistName:"Badshah",poster:"./Badshah/Posters/Dj-Waley-Babu.jpg",url:"./Badshah/Songs/Dj-Waley-Babu.mp3"},
+        {songName:"Genda Phool",artistName:"Badshah",poster:"./Badshah/Posters/Genda-phool.jpg",url:"./Badshah/Songs/Genda-Phool.mp3"},
+        {songName:"Gone Girl",artistName:"Badshah",poster:"./Badshah/Posters/Gone-girl.jpg",url:"/Badshah/Songs/Gone-Girl.mp3"},
+        {songName:"Heartless",artistName:"Badshah",poster:"./Badshah/Posters/Heartless.jpg",url:"/Badshah/Songs/Heartless.mp3"},
+        {songName:"Jugnu",artistName:"Badshah",poster:"./Badshah/Posters/jugnu.jpg",url:"/Badshah/Songs/Jugnu.mp3"},
+        {songName:"Mercy",artistName:"Badshah",poster:"./Badshah/Posters/mercy.jpg",url:"/Badshah/Songs/Mercy.mp3"},
+        {songName:"Oxygen",artistName:"Badshah",poster:"./Badshah/Posters/oxygen.jpg",url:"/Badshah/Songs/Oxygen.mp3"},
+        {songName:"Paagal",artistName:"Badshah",poster:"./Badshah/Posters/Paagal.jpg",url:"/Badshah/Songs/Paagal.mp3"},
+        {songName:"Paani-Paani",artistName:"Badshah",poster:"./Badshah/Posters/Paani-Paani.jpg",url:"/Badshah/Songs/Pani-Pani.mp3"},
+        {songName:"She Move It Like",artistName:"Badshah",poster:"./Badshah/Posters/she-move-it.jpg",url:"/Badshah/Songs/She-Move-It-like.mp3"}
+    ]},
+    {artistName:"Raftaar", img:"./Raftaar.jpg",song:[
+        {songName:"All Black",artistName:"Raftaar",poster:"./Raftaar/Posters/All-Black.jpg",url:"./Raftaar/Songs/All-Black.mp3"},
+        {songName:"Baby Marvake Manegi",artistName:"Raftaar",poster:"./Raftaar/Posters/Baby-Marvake.jpg",url:"./Raftaar/Songs/Baby-Marvake-Maanegi.mp3"},
+        {songName:"Chora Baba Ka",artistName:"Raftaar",poster:"./Raftaar/Posters/chora-baba-ka.jpg",url:"./Raftaar/Songs/Chora-Baba-Ka.mp3"},
+        {songName:"Ghana Kasoota",artistName:"Raftaar",poster:"./Raftaar/Posters/Ghana-Kasoota.jpg",url:"./Raftaar/Songs/Ghana-Kasoota.mp3"},
+        {songName:"Jean Teri",artistName:"Raftaar",poster:"./Raftaar/Posters/Jean-Teri.jpg",url:"./Raftaar/Songs/.mp3"},
+        {songName:"Lakdi Ki Kathi",artistName:"Raftaar",poster:"./Raftaar/Posters/Lakdi-Ki-Kaathi.jpg",url:"./Raftaar/Songs/Lakdi-Ki-Kathi.mp3"},
+        {songName:"Loca Loca",artistName:"Raftaar",poster:"./Raftaar/Posters/Loca-Loca.jpg",url:"./Raftaar/Songs/Loca-Loca.mp3"},
+        {songName:"Move",artistName:"Raftaar",poster:"./Raftaar/Posters/move.jpg",url:"./Raftaar/Songs/Move.mp3"},
+        {songName:"No Mercy",artistName:"Raftaar",poster:"./Raftaar/Posters/no-mercy.jpg",url:"./Raftaar/Songs/No-Mercy.mp3"},
+    ]},
+]
+
+
+var leftSongList = document.querySelector(".song-list");
+var popularArt = document.querySelector(".popular-art");
+var play = document.querySelector("#play");
+var forward = document.querySelector("#forward");
+var backward = document.querySelector("#backward");
+var myPlayList = document.querySelector("#my-playlist")
+var playlistName = document.querySelector('.playlist-name')
+var playBtn = document.querySelector('#play-btn')
+var selectedSong = 0;
+var flag = 0;
+var artistflag = 0;
+var playBtnFlag = 0;
+var audio = new Audio();
+// console.log(leftSongList)
+
+
+
+function musicList(){
+    var clutter = '';
+    songs.forEach(function(elem,idx){
+        clutter += `<div class="song" id=${idx}>
+        <img id=${idx}  src=${elem.poster}>
+        <div id=${idx} class="song-info">
+            <p id=${idx} class="song-name">${elem.songName}</p>
+            <p id=${idx} class="artist-name">${elem.artistName}</p>
+        </div>
+    </div>`
+    leftSongList.innerHTML = clutter;
+    audio.src = songs[selectedSong].url
+    
+    })
+}
+musicList();
+
+
+myPlayList.addEventListener('click',function(){
+    artistflag=0;
+    play.innerHTML = `<i class="ri-play-fill"></i>`
+    playlistName.textContent = "My PlayList";
+    musicList();
+    
+})
+
+playBtn.addEventListener('click',function(){
+    if (playBtnFlag == 0) {
+        audio.src = songs[songs.length-1].url
+        audio.play();
+        playBtn.textContent = "Pause"
+        play.innerHTML = `<i class="ri-pause-mini-fill"></i>`
+        playBtnFlag = 1
+    }else{
+        audio.pause();
+        playBtn.textContent = "Play"
+        play.innerHTML = `<i class="ri-play-fill"></i>`
+        playBtnFlag = 0;
+    }
+})
+
+
+function popularArtistList(){
+    var clutter = '';
+    popularArtist.forEach(function(elem,idx){
+        clutter += `<div id=${idx} class="artist">
+        <img id=${idx} src=${elem.img}>
+        <p id=${idx} class="Artist-name">${elem.artistName}</p>
+    </div>`
+    popularArt.innerHTML = clutter;
+    })
+}
+popularArtistList();
+
+
+var selectedSongs;
+popularArt.addEventListener('click',function(details){
+    artistflag = 1;
+    selectedArtist = details.target.id;
+    selectedSongs = popularArtist[selectedArtist].song
+    play.innerHTML = `<i class="ri-play-fill"></i>`
+    console.log(selectedSongs)
+    artistMusicList()
+    
+    
+    playlistName.textContent = popularArtist[selectedArtist].artistName;
+    
+})
+
+function artistMusicList(){
+    var clutter = '';
+    selectedSongs.forEach(function(elem,idx){
+        clutter += `<div class="song" id=${idx}>
+        <img id=${idx}  src=${elem.poster}>
+        <div id=${idx} class="song-info">
+            <p id=${idx} class="song-name">${elem.songName}</p>
+            <p id=${idx} class="artist-name">${elem.artistName}</p>
+        </div>
+    </div>`
+    leftSongList.innerHTML = clutter;
+    audio.src = selectedSongs[selectedSong].url
+    
+    })
+}
+// console.log(selectedSongs)
+
+
+
+leftSongList.addEventListener('click', function(details){
+    selectedSong = details.target.id;
+    if (artistflag == 0) {
+        play.innerHTML = `<i class="ri-pause-mini-fill"></i>`
+        musicList();
+        flag = 1;
+        audio.play()
+    }else{
+        play.innerHTML = `<i class="ri-pause-mini-fill"></i>`
+        artistMusicList()
+        flag = 1;
+        audio.play()
+    }
+    // console.log(details.target.id)
+})
+
+function artistmusicList(){
+    audio.src = songs[selectedSong].url
+}
+
+play.addEventListener('click',function(){
+    if(flag==0){
+        play.innerHTML = `<i class="ri-pause-mini-fill"></i>`
+        musicList()
+        audio.play()
+        flag =1
+    }else{
+        play.innerHTML = `<i class="ri-play-fill"></i>`
+        musicList()
+        audio.pause()
+        flag = 0
+    }
+})
+
+forward.addEventListener('click',function(){
+    if (selectedSong< songs.length-1) {
+        selectedSong++
+        musicList()
+        audio.play()
+        flag =1
+        play.innerHTML = `<i class="ri-pause-mini-fill"></i>`
+        backward.style.opacity =1;
+    }else{
+        forward.style.opacity= 0.3;
+    }
+})
+backward.addEventListener('click',function(){
+    if (selectedSong>0) {
+        selectedSong--
+        musicList()
+        audio.play()
+        flag =1
+        play.innerHTML = `<i class="ri-pause-mini-fill"></i>`
+        forward.style.opacity= 1;
+    }else{
+        backward.style.opacity =0.3;
+    }
+})
